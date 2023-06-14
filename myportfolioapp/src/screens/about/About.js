99dc49ce1a84c336018ab3fbfad9b9d3a3015dc0
TamilselvanChandrakumar <tamilbin001@gmail.com>
2023-06-14 10:41:48 +0530

    second

--- myportfolioapp/src/screens/about/About.js
+++ myportfolioapp/src/screens/about/About.js
@@ -6,7 +6,11 @@ export default function About() {
     <>
     <div className='about'>
       <div className='imagesec'></div>
-      <div className='aboutcontent'></div>
+      <div className='aboutcontent'>
+        <h1 className='title'>I'm Tamilselvan and I'm a <span className='webdev'>Webdeveloper</span> </h1><br></br>
+        <p className='detail'>I'm a fresher.I finished my BE-CSE in Shree Venkateshwara Hi-Tech Engineering college-Erode.I completed my internship (MERN stack course) in Kitkat Software Technologies at Coimbatore..And I am a person who exploring new Technologies..I have leadership qualities and work with the team efficiently.I'm a Hard working person and ability to work under pressure..</p><br></br>
+        <button className='cvbtn'>Download CV</button>
+      </div>
     </div>
     </>
   )
